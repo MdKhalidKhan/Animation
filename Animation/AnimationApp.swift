8667f8e7572290e1f52a1f25f793c9cfbb197326
Raj <rajkhulna@outlook.com>
2022-04-05 01:12:37 +0600

    Initial Commit

---
+++ Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Khalid Khan on 5/4/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
